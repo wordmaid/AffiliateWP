ce1851359726e8d472892914f91bd0f7865cbed7
Patrick Garman <patrick@garman.io>
2015-11-16 23:22:16 -0600

    First pass at simple registration options on user profile

--- affiliate-wp.php
+++ affiliate-wp.php
@@ -313,6 +313,7 @@ final class Affiliate_WP {
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/class-welcome.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/plugins.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/tools/class-migrate.php';
+			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/user-profile.php';
 
 		}
 

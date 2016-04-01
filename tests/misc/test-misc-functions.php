a6ac7dd61378bdc92dbc3d8a665e3a773165ec66
DrewAPicture <drew@10up.com>
2016-04-01 10:53:09 -0600

    Add tests for affwp_get_currency() and affwp_get_decimal_count().

--- tests/misc/test-misc-functions.php
+++ tests/misc/test-misc-functions.php
@@ -44,7 +44,7 @@ class Misc_Functions_Tests extends WP_UnitTestCase {
 	/**
 	 * @covers affwp_format_amount()
 	 */
-	public function test_affwp_format_amount_floatval_remains_floatval_with_comma_thousands_seperator() {
+	public function test_format_amount_floatval_remains_floatval_with_comma_thousands_seperator() {
 		affiliate_wp()->settings->set( array(
 			'thousands_separator' => ','
 		) );
@@ -57,5 +57,67 @@ class Misc_Functions_Tests extends WP_UnitTestCase {
 		$amount = affwp_format_amount( floatval( "1,999.99" ), true );
 
 		$this->assertSame( 'double', gettype( $this->amount ) );
+	}
+
+	/**
+	 * @covers affwp_get_currency()
+	 */
+	public function test_get_currency_should_default_to_USD() {
+		$currency = affwp_get_currency();
+
+		$this->assertEquals( 'USD', $currency );
+	}
+
+	/**
+	 * @covers affwp_get_currency()
+	 */
+	public function test_get_currency_modified_should_return_new_currency() {
+		$new = 'CZK';
+
+		affiliate_wp()->settings->set( array( 'currency' => $new ) );
+
+		$currency = affwp_get_currency();
+
+		$this->assertEquals( $new, $currency );
+	}
+
+	/**
+	 * @covers affwp_get_currency()
+	 */
+	public function test_get_currency_filtered_should_return_filtered_currency() {
+		$currency = affwp_get_currency();
+
+		$this->assertEquals( $currency, 'USD' );
+
+		add_filter( 'affwp_currency', function() {
+			return 'ZAR';
+		} );
+
+		$new_currency = affwp_get_currency();
+
+		$this->assertEquals( 'ZAR', $new_currency );
+		$this->assertNotEquals( 'USD', $new_currency );
+	}
+
+	/**
+	 * @covers affwp_get_decimal_count()
+	 */
+	public function test_get_decimal_count_default_should_be_2() {
+		$count = affwp_get_decimal_count();
+
+		$this->assertEquals( 2, $count );
+	}
+
+	/**
+	 * @covers affwp_get_decimal_count()
+	 */
+	public function test_filtered_get_decimal_count_should_return_filtered() {
+		add_filter( 'affwp_decimal_count', function() {
+			return 3;
+		} );
+
+		$count = affwp_get_decimal_count();
+
+		$this->assertEquals( 3, $count );
 	}
 }

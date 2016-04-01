2757b82085b7e5aa332269075ed8d495698421ca
DrewAPicture <drew@10up.com>
2016-04-01 10:52:33 -0600

    Add setUp() and tearDown() methods for misc-functions tests.

--- tests/misc/test-misc-functions.php
+++ tests/misc/test-misc-functions.php
@@ -15,6 +15,33 @@ class Misc_Functions_Tests extends WP_UnitTestCase {
 	public $amount;
 
 	/**
+	 * Settings instance.
+	 *
+	 * @since 1.8
+	 * @var Affiliate_WP_Settings
+	 */
+	public $settings;
+
+	/**
+	 * Set up.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->settings = new Affiliate_WP_Settings();
+	}
+
+	/**
+	 * Tear down.
+	 */
+	public function tearDown() {
+		// Reset to USD at the end of each test.
+		affiliate_wp()->settings->set( array( 'currency' => 'USD' ) );
+
+		parent::tearDown();
+	}
+
+	/**
 	 * @covers affwp_format_amount()
 	 */
 	public function test_affwp_format_amount_floatval_remains_floatval_with_comma_thousands_seperator() {

1dbf1fb2d3dd3a156907ccee05115fd14f04df70
DrewAPicture <drew@10up.com>
2016-04-03 12:35:18 -0600

    Add tests for affwp_get_affiliate_rate_type().

--- tests/affiliates/test-affiliate-functions.php
+++ tests/affiliates/test-affiliate-functions.php
@@ -357,8 +357,20 @@ class Affiliate_Functions_Tests extends WP_UnitTestCase {
 	/**
 	 * @covers affwp_get_affiliate_rate_type()
 	 */
-	function test_get_affiliate_rate_type() {
-		$this->assertEquals( 'percentage', affwp_get_affiliate_rate_type( $this->_affiliate_id ) );
+	public function test_get_affiliate_rate_type_default_should_be_percentage() {
+		$this->assertSame( 'percentage', affwp_get_affiliate_rate_type( $this->_affiliate_id ) );
+	}
+
+	/**
+	 * @covers affwp_get_affiliate_rate_type()
+	 */
+	public function test_get_affiliate_rate_type_filtered_should_not_match_default_percentage() {
+		add_filter( 'affwp_get_affiliate_rate_type', function() {
+			return 'flat';
+		} );
+
+		$this->assertSame( 'flat', affwp_get_affiliate_rate_type() );
+		$this->assertNotSame( 'percentage', affwp_get_affiliate_rate_type() );
 	}
 
 	/**

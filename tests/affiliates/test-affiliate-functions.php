68da5a694f9db531f3de63a0fd53997f4e22b3cc
DrewAPicture <drew@10up.com>
2016-04-04 00:12:43 -0600

    Add test for invalid affiliate id and object for affwp_get_affiliate_conversion_rate().

--- tests/affiliates/test-affiliate-functions.php
+++ tests/affiliates/test-affiliate-functions.php
@@ -1122,6 +1122,20 @@ class Affiliate_Functions_Tests extends WP_UnitTestCase {
 	/**
 	 * @covers affwp_get_affiliate_conversion_rate()
 	 */
+	public function test_get_affiliate_conversion_rate_with_invalid_affiliate_id_should_return_false() {
+		$this->assertFalse( affwp_get_affiliate_conversion_rate( null ) );
+	}
+
+	/**
+	 * @covers affwp_get_affiliate_conversion_rate()
+	 */
+	public function test_get_affiliate_conversion_rate_with_invalid_affiliate_object_should_return_false() {
+		$this->assertFalse( affwp_get_affiliate_conversion_rate( new stdClass() ) );
+	}
+
+	/**
+	 * @covers affwp_get_affiliate_conversion_rate()
+	 */
 	function test_get_affiliate_conversion_rate() {
 		$this->assertEquals( '0%', affwp_get_affiliate_conversion_rate( $this->_affiliate_id ) );
 	}

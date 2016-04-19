4383d3a58cd614abe98498fbb037ba51dfd5c841
Rami Abraham <r@radesign.ws>
2016-04-19 14:13:04 -0400

    Add js method for collapsing reports metaboxes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,24 +1,24 @@
-jQuery(document).ready(function($) {
-    // Settings uploader
+jQuery( document ).ready( function( $ ) {
+	// Settings uploader
 	var file_frame;
 	window.formfield = '';
 
-	$('body').on('click', '.affwp_settings_upload_button', function(e) {
+	$( 'body' ).on( 'click', '.affwp_settings_upload_button', function( e ) {
 
 		e.preventDefault();
 
-		var button = $(this);
+		var button = $( this );
 
-		window.formfield = $(this).parent().prev();
+		window.formfield = $( this ).parent().prev();
 
 		// If the media frame already exists, reopen it.
-		if( file_frame ) {
+		if ( file_frame ) {
 			file_frame.open();
 			return;
 		}
 
 		// Create the media frame
-		file_frame = wp.media.frames.file_frame = wp.media({
+		file_frame = wp.media.frames.file_frame = wp.media( {
 			frame: 'post',
 			state: 'insert',
 			title: button.data( 'uploader_title' ),
@@ -26,7 +26,7 @@ jQuery(document).ready(function($) {
 				text: button.data( 'uploader_button_text' )
 			},
 			multiple: false
-		});
+		} );
 
 		file_frame.on( 'menu:render:default', function( view ) {
 			// Store our views in an object,
@@ -40,7 +40,7 @@ jQuery(document).ready(function($) {
 
 			// Initialize the views in our view object
 			view.set( views );
-		});
+		} );
 
 		// When an image is selected, run a callback
 		file_frame.on( 'insert', function() {
@@ -48,47 +48,48 @@ jQuery(document).ready(function($) {
 
 			selection.each( function( attachment, index ) {
 				attachment = attachment.toJSON();
-				window.formfield.val(attachment.url);
-			});
-		});
+				window.formfield.val( attachment.url );
+			} );
+		} );
 
 		// Open the modal
 		file_frame.open();
-	});
+	} );
 
 	var file_frame;
 	window.formfield = '';
 
 	// Show referral export form
-	$('.affwp-referrals-export-toggle').click(function() {
-		$('.affwp-referrals-export-toggle').toggle();
-		$('#affwp-referrals-export-form').slideToggle();
-	});
+	$( '.affwp-referrals-export-toggle' ).click( function() {
+		$( '.affwp-referrals-export-toggle' ).toggle();
+		$( '#affwp-referrals-export-form' ).slideToggle();
+	} );
 
-	$('#affwp-referrals-export-form').submit(function() {
-		if( ! confirm( affwp_vars.confirm ) ) {
+	$( '#affwp-referrals-export-form' ).submit( function() {
+		if ( !confirm( affwp_vars.confirm ) ) {
 			return false;
 		}
-	});
+	} );
 
 	// datepicker
-	if( $('.affwp-datepicker').length ) {
-		$('.affwp-datepicker').datepicker();
+	if ( $( '.affwp-datepicker' ).length ) {
+		$( '.affwp-datepicker' ).datepicker();
 	}
 
 	var user_search_delay;
 
 	// ajax user search
-	$('body').on( 'input change', '.affwp-user-search', function() {
+	$( 'body' ).on( 'input change', '.affwp-user-search', function() {
 		clearTimeout( user_search_delay );
 
-		$('.affwp-ajax').hide();
+		$( '.affwp-ajax' ).hide();
 
-		var user_search = $(this).val(), status = $(this).data('affwp-status');
+		var user_search = $( this ).val(),
+			status = $( this ).data( 'affwp-status' );
 
 		// delay search 500ms between keypress for performance
 		user_search_delay = setTimeout( function() {
-			$('.affwp-ajax').show();
+			$( '.affwp-ajax' ).show();
 
 			data = {
 				action: 'affwp_search_users',
@@ -96,42 +97,43 @@ jQuery(document).ready(function($) {
 				status: status
 			};
 
-			$.ajax({
+			$.ajax( {
 				type: "POST",
 				data: data,
 				dataType: "json",
 				url: ajaxurl,
-				success: function (search_response) {
-					$('.affwp-ajax').hide();
+				success: function( search_response ) {
+					$( '.affwp-ajax' ).hide();
 
-					$('#affwp_user_search_results').html('');
+					$( '#affwp_user_search_results' ).html( '' );
 
-					$(search_response.results).appendTo('#affwp_user_search_results');
+					$( search_response.results ).appendTo( '#affwp_user_search_results' );
 
-					if( $('.affwp-woo-coupon-field').length ) {
-						var height = $('.affwp-woo-coupon-field #affwp_user_search_results' ).height();
-						$('.affwp-woo-coupon-field #affwp_user_search_results').css('top', '-' + height + 'px' );
+					if ( $( '.affwp-woo-coupon-field' ).length ) {
+						var height = $( '.affwp-woo-coupon-field #affwp_user_search_results' ).height();
+						$( '.affwp-woo-coupon-field #affwp_user_search_results' ).css( 'top', '-' + height + 'px' );
 					}
 				}
-			});
-		}, 500);
-	});
+			} );
+		}, 500 );
+	} );
 
-	$('body').on('click.rcpSelectUser', '#affwp_user_search_results a', function(e) {
+	$( 'body' ).on( 'click.rcpSelectUser', '#affwp_user_search_results a', function( e ) {
 		e.preventDefault();
-		var login = $(this).data('login'), id = $(this).data('id');
-		$('#user_name').val(login);
-		$('#user_id').val(id);
-		$('#affwp_user_search_results').html('');
-	});
+		var login = $( this ).data( 'login' ),
+			id = $( this ).data( 'id' );
+		$( '#user_name' ).val( login );
+		$( '#user_id' ).val( id );
+		$( '#affwp_user_search_results' ).html( '' );
+	} );
 
 	// select image for creative
 	var file_frame;
-	$('body').on('click', '.upload_image_button', function(e) {
+	$( 'body' ).on( 'click', '.upload_image_button', function( e ) {
 
 		e.preventDefault();
 
-		var formfield = $(this).prev();
+		var formfield = $( this ).prev();
 
 		// If the media frame already exists, reopen it.
 		if ( file_frame ) {
@@ -141,7 +143,7 @@ jQuery(document).ready(function($) {
 		}
 
 		// Create the media frame.
-		file_frame = wp.media.frames.file_frame = wp.media({
+		file_frame = wp.media.frames.file_frame = wp.media( {
 			frame: 'select',
 			title: 'Choose Image',
 			multiple: false,
@@ -151,56 +153,56 @@ jQuery(document).ready(function($) {
 			button: {
 				text: 'Use Image'
 			}
-		});
+		} );
 
-		file_frame.on( 'menu:render:default', function(view) {
-	        // Store our views in an object.
-	        var views = {};
+		file_frame.on( 'menu:render:default', function( view ) {
+			// Store our views in an object.
+			var views = {};
 
-	        // Unset default menu items
-	        view.unset('library-separator');
-	        view.unset('gallery');
-	        view.unset('featured-image');
-	        view.unset('embed');
+			// Unset default menu items
+			view.unset( 'library-separator' );
+			view.unset( 'gallery' );
+			view.unset( 'featured-image' );
+			view.unset( 'embed' );
 
-	        // Initialize the views in our view object.
-	        view.set(views);
-	    });
+			// Initialize the views in our view object.
+			view.set( views );
+		} );
 
 		// When an image is selected, run a callback.
 		file_frame.on( 'select', function() {
-			var attachment = file_frame.state().get('selection').first().toJSON();
-			formfield.val(attachment.url);
+			var attachment = file_frame.state().get( 'selection' ).first().toJSON();
+			formfield.val( attachment.url );
 
-			var img = $('<img />');
-			img.attr('src', attachment.url);
+			var img = $( '<img />' );
+			img.attr( 'src', attachment.url );
 			// replace previous image with new one if selected
-			$('#preview_image').empty().append( img );
+			$( '#preview_image' ).empty().append( img );
 
 			// show preview div when image exists
-			if ( $('#preview_image img') ) {
-				$('#preview_image').show();
+			if ( $( '#preview_image img' ) ) {
+				$( '#preview_image' ).show();
 			}
-		});
+		} );
 
 		// Finally, open the modal
 		file_frame.open();
-	});
+	} );
 
 	// Confirm referral deletion
-	$('body').on('click', '.affiliates_page_affiliate-wp-referrals .delete', function(e) {
+	$( 'body' ).on( 'click', '.affiliates_page_affiliate-wp-referrals .delete', function( e ) {
 
-		if( confirm( affwp_vars.confirm_delete_referral) ) {
+		if ( confirm( affwp_vars.confirm_delete_referral ) ) {
 			return true;
 		}
 
 		return false;
 
-	});
+	} );
 
 	function maybe_activate_migrate_users_button() {
-		var checked = $('#affiliate-wp-migrate-user-accounts input:checkbox:checked' ).length,
-		    $button = $('#affiliate-wp-migrate-user-accounts input[type=submit]');
+		var checked = $( '#affiliate-wp-migrate-user-accounts input:checkbox:checked' ).length,
+			$button = $( '#affiliate-wp-migrate-user-accounts input[type=submit]' );
 
 		if ( checked > 0 ) {
 			$button.prop( 'disabled', false );
@@ -211,20 +213,25 @@ jQuery(document).ready(function($) {
 
 	maybe_activate_migrate_users_button();
 
-	$('body').on('change', '#affiliate-wp-migrate-user-accounts input:checkbox', function() {
+	$( 'body' ).on( 'change', '#affiliate-wp-migrate-user-accounts input:checkbox', function() {
 		maybe_activate_migrate_users_button();
-	});
+	} );
 
-	$('#affwp_add_affiliate #status').change(function() {
+	$( '#affwp_add_affiliate #status' ).change( function() {
 
-		var status = $(this).val();
-		if( 'active' == status ) {
-			$('#affwp-welcome-email-row').show();
+		var status = $( this ).val();
+		if ( 'active' == status ) {
+			$( '#affwp-welcome-email-row' ).show();
 		} else {
-			$('#affwp-welcome-email-row').hide();
-			$('#affwp-welcome-email-row #welcome_email').prop( 'checked', false );
+			$( '#affwp-welcome-email-row' ).hide();
+			$( '#affwp-welcome-email-row #welcome_email' ).prop( 'checked', false );
 		}
 
-	});
+	} );
 
-});
+	$( '.reports-metabox-holder .button-link' ).on( 'click', function( e ) {
+		e.preventDefault();
+		$( this ).siblings( '.postbox .inside' ).toggle();
+	} );
+
+} );

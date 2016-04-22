f419a997d920f0d03526128d218b9289fc9e0ea1
DrewAPicture <drew@10up.com>
2016-04-22 09:58:43 -0600

    Implement the AffWP_Referral class as an extension of AffWP_Object.

---
+++ includes/objects/class-affwp-referral.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Drew
+ * Date: 4/22/16
+ * Time: 9:48 AM
+ */
+class AffWP_Referral extends AffWP_Object {
+
+	/**
+	 * Referral ID.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var int
+	 */
+	public $referral_id = 0;
+
+	/**
+	 * Affiliate ID.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var int
+	 */
+	public $affiliate_id = 0;
+
+	/**
+	 * Visit ID.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var int
+	 */
+	public $visit_id = 0;
+
+	/**
+	 * Referral description.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $description;
+
+	/**
+	 * Referral status.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $status;
+
+	/**
+	 * Referral amount.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $amount;
+
+	/**
+	 * Referral currency.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $currency;
+
+	/**
+	 * Custom referral data.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $custom;
+
+	/**
+	 * Referral context (usually integration).
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $context;
+
+	/**
+	 * Referral campaign name.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $campaign;
+
+	/**
+	 * Referral reference.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $reference;
+
+	/**
+	 * Products associated with the referral.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $products;
+
+	/**
+	 * Date the referral was created.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @var string
+	 */
+	public $date;
+
+	/**
+	 * Token to use for generating cache keys.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @static
+	 * @var string
+	 */
+	public static $cache_token = 'affwp_referrals';
+
+	/**
+	 * Referrals cache group.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @static
+	 * @var string
+	 */
+	public static $cache_group = 'creative';
+
+	/**
+	 * Retrieves a referral based on a referral ID.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @static
+	 *
+	 * @param int $referral_id Referral ID.
+	 * @return AffWP_Referral|null Affiliate object or null if it doesn't exist,
+	 */
+	public static function get( $referral_id ) {
+		return affiliate_wp()->referrals->get( $referral_id );
+	}
+
+	/**
+	 * Sanitizes a referral object field.
+	 *
+	 * @since 1.9
+	 * @access public
+	 * @static
+	 *
+	 * @param string $field        Object field.
+	 * @param mixed  $value        Field value.
+	 * @return mixed Sanitized field value.
+	 */
+	public static function sanitize_field( $field, $value ) {
+		if ( in_array( $field, array( 'referral_id', 'affiliate_id', 'visit_id' ) ) ) {
+			$value = (int) $value;
+		}
+		return $value;
+	}
+
+}

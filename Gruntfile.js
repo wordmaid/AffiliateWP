8b8252cd23352f46b8b08a20a1d079c1a94e042e
Rami Abraham <r@radesign.ws>
2016-05-17 14:43:42 -0400

    Add grunt task for docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
 /* global require, process */
 module.exports = function( grunt ) {
 	// Load Grunt plugin configurations
-	require('load-grunt-config')(grunt, {
+	require( 'load-grunt-config' )( grunt, {
 		data: {
-			pkg: grunt.file.readJSON('package.json')
+			pkg: grunt.file.readJSON( 'package.json' )
 		}
-	});
+	} );
+
+	grunt.initConfig( {
+		exec: {
+			make_docs: {
+				cmd: './docs.sh'
+			}
+		}
+	} );
+
+	grunt.registerTask( 'docs', [ 'exec:make_docs' ] );
 };

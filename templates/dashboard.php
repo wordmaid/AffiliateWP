e720590837f8a22d612b57466ad4ecbb80cc2287
Andrew Munro <andrew@sumobi.com>
2016-05-26 03:06:41 +1200

    Pass $active_tab to affwp_affiliate_dashboard_top, *_notices, and *_bottom hooks (#1307)
    Fix #1306
    
    * Add a hook doc + changelog entry for the new parameter.
    
    * Pass active_tab to the *_dashboard_top and *_dashboard_notices hooks. Adds hook docs with changelog entries.
    
    * affwp_get_affiliate_id() can also return false.

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -18,7 +18,18 @@
 
 	<?php if ( 'active' == affwp_get_affiliate_status( affwp_get_affiliate_id() ) ) : ?>
 
-		<?php do_action( 'affwp_affiliate_dashboard_top', affwp_get_affiliate_id() ); ?>
+		<?php
+		/**
+		 * Fires at the top of the affiliate dashboard.
+		 *
+		 * @since 0.2
+		 * @since 1.8.2 Added the `$active_tab` parameter.
+		 *
+		 * @param int|false $affiliate_id ID for the current affiliate.
+		 * @param string    $active_tab   Slug for the currently-active tab.
+		 */
+		do_action( 'affwp_affiliate_dashboard_top', affwp_get_affiliate_id(), $active_tab );
+		?>
 
 		<?php if ( ! empty( $_GET['affwp_notice'] ) && 'profile-updated' == $_GET['affwp_notice'] ) : ?>
 
@@ -26,7 +37,18 @@
 
 		<?php endif; ?>
 
-		<?php do_action( 'affwp_affiliate_dashboard_notices', affwp_get_affiliate_id() ); ?>
+		<?php
+		/**
+		 * Fires inside the affiliate dashboard above the tabbed interface.
+		 *
+		 * @since 0.2
+		 * @since 1.8.2 Added the `$active_tab` parameter.
+		 *
+		 * @param int|false $affiliate_id ID for the current affiliate.
+		 * @param string    $active_tab   Slug for the currently-active tab.
+		 */
+		do_action( 'affwp_affiliate_dashboard_notices', affwp_get_affiliate_id(), $active_tab );
+		?>
 
 		<ul id="affwp-affiliate-dashboard-tabs">
 			<?php if ( affwp_affiliate_area_show_tab( 'urls' ) ) : ?>
@@ -79,7 +101,18 @@
 		endif;
 		?>
 
-		<?php do_action( 'affwp_affiliate_dashboard_bottom', affwp_get_affiliate_id() ); ?>
+		<?php
+		/**
+		 * Fires at the bottom of the affiliate dashboard.
+		 *
+		 * @since 0.2
+		 * @since 1.8.2 Added the `$active_tab` parameter.
+		 *
+		 * @param int|false $affiliate_id ID for the current affiliate.
+		 * @param string    $active_tab   Slug for the currently-active tab.
+		 */
+		do_action( 'affwp_affiliate_dashboard_bottom', affwp_get_affiliate_id(), $active_tab );
+		?>
 
 	<?php endif; ?>
 

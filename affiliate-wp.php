4bd74b1f13fba2f7682caea002b23fcf765d26d3
Rami Abraham <r@radesign.ws>
2016-06-14 17:55:21 -0400

    Adjust inc path to correct new graphs dir

--- affiliate-wp.php
+++ affiliate-wp.php
@@ -330,8 +330,8 @@ final class Affiliate_WP {
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/emails/functions.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/emails/actions.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-graph.php';
-		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-referrals-graph.php';
-		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-visits-graph.php';
+		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/graphs/class-referrals-graph.php';
+		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/graphs/class-visits-graph.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-integrations.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-login.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-logging.php';

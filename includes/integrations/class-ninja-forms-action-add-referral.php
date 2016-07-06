42972871e35a552df27bb86bf59ebae2b93ecd90
Rami Abraham <r@radesign.ws>
2016-07-06 13:19:18 -0400

    Comments, formatting for NF Action class

--- includes/integrations/class-ninja-forms-action-add-referral.php
+++ includes/integrations/class-ninja-forms-action-add-referral.php
@@ -1,35 +1,36 @@
-<?php if ( ! defined( 'ABSPATH' ) || ! class_exists( 'NF_Abstracts_Action' )) exit;
+<?php if ( ! defined( 'ABSPATH' ) || ! class_exists( 'NF_Abstracts_Action' ) ) exit;
 
-/**
- * Class Affiliate_WP_Ninja_Forms_ActionAddReferral
- */
-final class Affiliate_WP_Ninja_Forms_ActionAddReferral extends NF_Abstracts_Action
-{
+final class Affiliate_WP_Ninja_Forms_Add_Referral extends NF_Abstracts_Action {
     /**
-     * @var string
+     * @var   string
+     * @since 1.8.4
      */
     protected $_name  = 'affiliatewp_add_referral';
 
     /**
-     * @var array
+     * @var   array
+     * @since 1.8.4
      */
     protected $_tags = array( 'affiliate', 'affiliatewp', 'referral' );
 
     /**
-     * @var string
+     * @var   string
+     * @since 1.8.4
      */
     protected $_timing = 'late';
 
     /**
-     * @var int
+     * @var   int
+     * @since 1.8.4
      */
     protected $_priority = '10';
 
     /**
      * Constructor
+     *
+     * @see  [ninja-forms]/includes/abtracts/Action
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
 
         $this->_nicename = __( 'Add Referral', 'ninja-forms' );
@@ -59,9 +60,9 @@ final class Affiliate_WP_Ninja_Forms_ActionAddReferral extends NF_Abstracts_Acti
             'group' => 'primary',
             'use_merge_tags' => array(
                 'exclude' => array(
-                    'user',
+                    'user'
                 )
-            ),
+            )
         );
 
         $this->_settings[ 'affiliatewp_description' ] = array(
@@ -70,24 +71,37 @@ final class Affiliate_WP_Ninja_Forms_ActionAddReferral extends NF_Abstracts_Acti
             'type' => 'textbox',
             'width' => 'full',
             'value' => '',
-            'group' => 'advanced',
+            'group' => 'advanced'
         );
     }
 
-    /*
-    * PUBLIC METHODS
-    */
-
-    public function save( $action_settings )
-    {
-
+    /**
+     * Public save method
+     *
+     * @since  1.8.4
+     *
+     * @param  array  $action_settings  [description]
+     *
+     */
+    public function save( $action_settings ) {
     }
 
-    public function process( $action_settings, $form_id, $data )
-    {
+    /**
+     * Processes the AffiliateWP referral
+     *
+     * @since  1.8.4
+     *
+     * @param  array   $action_settings Form action settings
+     * @param  int     $form_id         The form ID
+     * @param  mixed   $data            Form data
+     *
+     * @return mixed   $data            Form data
+     *
+     */
+    public function process( $action_settings, $form_id, $data ) {
         $referral_total = $this->get_total( $action_settings );
-        $reference = $this->get_reference( $data );
-        $description = $this->get_description( $action_settings, $data );
+        $reference      = $this->get_reference( $data );
+        $description    = $this->get_description( $action_settings, $data );
         $customer_email = $this->get_customer_email( $action_settings );
 
         $args = $data[ 'extra' ][ 'affiliatewp' ] = compact( 'referral_total', 'reference', 'description', 'customer_email' );
@@ -97,46 +111,68 @@ final class Affiliate_WP_Ninja_Forms_ActionAddReferral extends NF_Abstracts_Acti
         return $data;
     }
 
-    /*
-    * PRIVATE METHODS
-    */
 
-    private function get_total( $action_settings )
-    {
+    /**
+     * Get the total of the form
+     *
+     * @since  1.8.4
+     *
+     * @param  array  $action_settings  The form action settings
+     *
+     * @return int                      Total amount of the form
+     */
+    private function get_total( $action_settings ) {
         $total = 0;
-        if( isset( $action_settings[ 'affiliatewp_total' ] ) ){
+        if( isset( $action_settings[ 'affiliatewp_total' ] ) ) {
             $total = $action_settings[ 'affiliatewp_total' ];
         }
         return $total;
     }
 
-    private function get_reference( $data )
-    {
+    /**
+     * Get the referral reference
+     *
+     * @since  1.8.4
+     *
+     * @param  mixed   $data       Form data
+     *
+     * @return string  $reference  The referral reference
+     */
+    private function get_reference( $data ) {
         $reference = '';
-        if( isset( $data[ 'actions' ][ 'save' ][ 'id' ] ) ){
+        if( isset( $data[ 'actions' ][ 'save' ][ 'id' ] ) ) {
             $reference = $data[ 'actions' ][ 'save' ][ 'id' ];
         }
         return $reference;
     }
 
-    private function get_description( $action_settings, $data )
-    {
+    /**
+     * Get the referral description
+     *
+     * @since  1.8.4
+     *
+     * @param  array   $action_settings  The form action settings
+     * @param  mixed   $data             Form data
+     *
+     * @return string  $description      The referral description. Will first check for
+     *                                   an AffiliateWP custom description. Uses the form
+     *                                   title if no custom description is defined.
+     */
+    private function get_description( $action_settings, $data ) {
         $description = '';
-        if( isset( $action_settings[ 'affiliatewp_description' ] ) ){
+        if( isset( $action_settings[ 'affiliatewp_description' ] ) ) {
             $description = $action_settings[ 'affiliatewp_description' ];
-        } elseif( isset( $data[ 'settings' ][ 'title' ] ) ){
+        } elseif( isset( $data[ 'settings' ][ 'title' ] ) ) {
             $description = $data[ 'settings' ][ 'title' ];
         }
         return $description;
     }
 
-    private function get_customer_email( $action_settings )
-    {
+    private function get_customer_email( $action_settings ) {
         $email = 0;
-        if( isset( $action_settings[ 'affiliatewp_email' ] ) ){
+        if( isset( $action_settings[ 'affiliatewp_email' ] ) ) {
             $email = $action_settings[ 'affiliatewp_email' ];
         }
         return $email;
     }
-
 }

cdacd95ae7040380572243032f424995a99b258c
Rami Abraham <r@radesign.ws>
2016-07-07 18:58:36 -0400

    Save meta box order and open states

--- includes/admin/overview/overview.php
+++ includes/admin/overview/overview.php
@@ -49,7 +49,12 @@ function affwp_affiliates_dashboard() { ?>
 			</div>
 		</div>
 
-		<?php do_action( 'affwp_overview_bottom' ); ?>
+		<?php do_action( 'affwp_overview_bottom' );
+
+		wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce' , false );
+		wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce' , false );
+
+		?>
 
 	</div>
 <?php }

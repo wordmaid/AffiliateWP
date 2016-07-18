c48d5ff7d15434d7207d93efa11b51f75f607381
Drew Jaynes <DrewAPicture@users.noreply.github.com>
2016-07-18 09:41:08 -0600

    issue/1444 – Improve spacing and update associated comment

--- includes/emails/actions.php
+++ includes/emails/actions.php
@@ -86,12 +86,17 @@ function affwp_notify_on_approval( $affiliate_id = 0, $status = '', $old_status 
 		return;
 	}
 
-	/**
-	 * Only send email if:
-	 * Affiliate approval is disabled ( $status = active, $old_status = active )
-	 * Affiliate approval is enabled and the affiliate is accepted ( $status = active, $old_status = pending )
+	/*
+	 * Only send approval email if:
+	 *
+	 * - Affiliate was inactive now active
+	 * - Affiliate was pending now active
+	 * - Affiliate was rejected now active
 	 */
-	if ( ! ( 'active' == $status && 'inactive' == $old_status || 'active' == $status && 'pending' == $old_status || 'active' == $status && 'rejected' == $old_status ) ) {
+	if (! ( 'active' == $status && 'inactive' == $old_status )
+		|| ( 'active' == $status && 'pending' == $old_status )
+		|| ( 'active' == $status && 'rejected' == $old_status )
+	) {
 		return;
 	}
 

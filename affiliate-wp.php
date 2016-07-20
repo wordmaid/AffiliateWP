d17c44d51d443c922a1ce5f489833786c84a6925
DrewAPicture <drew@10up.com>
2016-07-19 18:40:00 -0600

    Update affiliate-wp.php from release/1.9.

--- affiliate-wp.php
+++ affiliate-wp.php
@@ -309,7 +309,6 @@ final class Affiliate_WP {
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/class-notices.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/creatives/actions.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/creatives/creatives.php';
-			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/class-metabox-base.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/overview/overview.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/referrals/actions.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/referrals/referrals.php';

21e323895bb0d5e77e6e980461a0ef3853279894
Rami Abraham <r@radesign.ws>
2016-07-26 17:30:53 -0400

    Merge release/1.9 into issue/1225

--- tests/test-capabilities.php
+++ tests/test-capabilities.php
@@ -2,20 +2,31 @@
 
 class Capabilities_Tests extends WP_UnitTestCase {
 
-	protected $_user_id = 1;
+	protected $_user_id;
 
-	function setUp() {
+	/**
+	 * Set up.
+	 */
+	public function setUp() {
 		parent::setUp();
+		
+		$this->_user_id = $this->factory->user->create( array(
+			'role' => 'administrator'
+		) );
+
+		// Flush the $wp_roles global.
+		$this->_flush_roles();
 	}
 
-	function test_admin_has_caps() {
-
-		$this->markTestIncomplete( 'Fails 50% of the time. No idea why' );
+	/**
+	 * @covers Affiliate_WP_Capabilities
+	 */
+	public function test_admin_has_caps() {
 
 		$roles = new Affiliate_WP_Capabilities;
 		$roles->add_caps();
 
-		$user = new WP_User( $this->_user_id );
+		$user = get_user_by( 'id', $this->_user_id );
 
 		$this->assertTrue( $user->has_cap( 'view_affiliate_reports' ) );
 		$this->assertTrue( $user->has_cap( 'export_affiliate_data' ) );
@@ -25,6 +36,21 @@ class Capabilities_Tests extends WP_UnitTestCase {
 		$this->assertTrue( $user->has_cap( 'manage_visits' ) );
 		$this->assertTrue( $user->has_cap( 'manage_creatives' ) );
 
+	}
+
+	/**
+	 * Helper to flush the $wp_roles global.
+	 */
+	public function _flush_roles() {
+		/*
+		 * We want to make sure we're testing against the db, not just in-memory data
+		 * this will flush everything and reload it from the db
+		 */
+		unset($GLOBALS['wp_user_roles']);
+		global $wp_roles;
+		if ( is_object( $wp_roles ) ) {
+			$wp_roles->_init();
+		}
 	}
 
 }

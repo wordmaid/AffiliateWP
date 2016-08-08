65581237a01ff7c6fe9c8bc4521f684568fb57a8
Drew Jaynes <drew@affiliatewp.com>
2016-08-08 15:13:22 -0600

    Remove an extra unneded file, tests/phpunit/factory.php and simply require files directly in affwp-testscase.php.

--- tests/phpunit/affwp-testcase.php
+++ tests/phpunit/affwp-testcase.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once dirname( __FILE__ ) . '/factory.php';
+// Load factory files.
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-affiliates.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-creatives.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-referrals.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-visits.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory.php' );
 
 /**
  * Defines a basic fixture to run multiple tests.

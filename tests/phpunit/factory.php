dc2e5a9f8c63c533b2a387a3b47202d92fc1b8a8
Drew Jaynes <drew@affiliatewp.com>
2016-08-08 15:16:52 -0600

    Revert "Remove an extra unneded file, tests/phpunit/factory.php and simply require files directly in affwp-testscase.php."
    This reverts commit 65581237a01ff7c6fe9c8bc4521f684568fb57a8.

---
+++ tests/phpunit/factory.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-affiliates.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-creatives.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-referrals.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory-for-visits.php' );
+require_once( dirname( __FILE__ ) . '/factory/class-affwp-factory.php' );

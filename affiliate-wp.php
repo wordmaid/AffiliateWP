184994dccf2064a8ffabb7f2b541a7365171b8e5
Pippin Williamson <pippin@pippinsplugins.com>
2016-09-21 08:55:52 -0500

    issue/1562 - Remove welcome screen and class file.  (#1624)
    * Remove welcome screen and class file. #1562
    
    * Add changelog link to plugin meta and remove getting started. #1562

--- affiliate-wp.php
+++ affiliate-wp.php
@@ -359,7 +359,6 @@ final class Affiliate_WP {
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/visits/visits.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/tools/tools.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/class-upgrades.php';
-			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/class-welcome.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/plugins.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/tools/class-migrate.php';
 			require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/add-ons.php';

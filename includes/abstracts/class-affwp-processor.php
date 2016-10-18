c3339ba34928839b0bb552bbef830037d790cb69
Drew Jaynes <drew@affiliatewp.com>
2016-10-18 13:19:37 -0600

    Add basis for Ajax processing a single item in the batch.

--- includes/abstracts/class-affwp-processor.php
+++ includes/abstracts/class-affwp-processor.php
@@ -15,7 +15,8 @@ abstract class Batch_Processor {
 	 * @since  2.0
 	 */
 	public function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'localize_script' ) );
+		add_action( 'wp_enqueue_scripts',          array( $this, 'localize_script' ) );
+		add_action( 'wp_ajax_process_single_item', array( $this, 'process_item'    ) );
 	}
 
 	/**
@@ -25,7 +26,19 @@ abstract class Batch_Processor {
 	 * @since  2.0
 	 */
 	public function localize_script() {
-		wp_localize_script( 'admin', 'affwpl10n', $this->js_vars() );
+		wp_localize_script( 'admin', 'affwpl10n', $this->get_js_vars() );
+	}
+
+	/**
+	 * Retrieves the list of registered JS vars.
+	 *
+	 * @access private
+	 * @since  2.0
+	 *
+	 * @return array Array of JS vars to print.
+	 */
+	private function get_js_vars() {
+		return $this->js_vars();
 	}
 
 	/**
@@ -38,4 +51,13 @@ abstract class Batch_Processor {
 	 * @return array Array of variables to pass when localizing the script.
 	 */
 	abstract public function js_vars();
+
+	/**
+	 * Handles processing a single item and returning a JSON response to the Ajax script.
+	 *
+	 * @access public
+	 * @since  2.0
+	 * @abstract
+	 */
+	abstract public function process_item();
 }

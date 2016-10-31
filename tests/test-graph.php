dfaba088aee95fbca4e346d6094f72073d9b4036
Drew Jaynes <drew@affiliatewp.com>
2016-10-31 16:59:03 -0600

    Supplement graph date tests.

--- tests/test-graph.php
+++ tests/test-graph.php
@@ -9,10 +9,6 @@ use AffWP\Tests\UnitTestCase;
  * @covers Affiliate_WP_Graph
  * @group graph
  */
-
-
-
-
 class Tests extends UnitTestCase {
 
 	/**
@@ -20,21 +16,16 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_get_report_dates_correct_this_month_at_the_end_of_the_month_utc() {
 
-		global $AFFWP_TEST_TIME, $AFFWP_TEST_UTC_OFFSET;
-
-		$AFFWP_TEST_TIME       = strtotime( '2016/08/31 23:59 UTC' );
-		$AFFWP_TEST_UTC_OFFSET = 0;
-
 		date_default_timezone_set( 'UTC' );
 
 		$dates = affwp_get_report_dates();
 
 		$this->assertEquals( $dates['day'], 1 );
-		$this->assertEquals( $dates['m_start'], 8 );
-		$this->assertEquals( $dates['year'], 2016 );
-		$this->assertEquals( $dates['day_end'], 31 );
-		$this->assertEquals( $dates['m_end'], 8 );
-		$this->assertEquals( $dates['year_end'], 2016 );
+		$this->assertEquals( $dates['m_start'], date( 'n' ) );
+		$this->assertEquals( $dates['year'], date( 'Y' ) );
+		$this->assertEquals( $dates['day_end'], cal_days_in_month( CAL_GREGORIAN, $dates['m_start'], $dates['year'] ) );
+		$this->assertEquals( $dates['m_end'], date( 'n' ) );
+		$this->assertEquals( $dates['year_end'], date( 'Y' ) );
 
 	}
 
@@ -43,21 +34,16 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_get_report_dates_correct_this_month_at_the_end_of_the_month_nz() {
 
-		global $AFFWP_TEST_TIME, $AFFWP_TEST_UTC_OFFSET;
-
-		$AFFWP_TEST_TIME       = strtotime( '2016/08/31 12:00 UTC' );
-		$AFFWP_TEST_UTC_OFFSET = 12;
-
 		date_default_timezone_set( 'Pacific/Auckland' );
 
 		$dates = affwp_get_report_dates();
 
 		$this->assertEquals( $dates['day'], 1 );
-		$this->assertEquals( $dates['m_start'], 9 );
-		$this->assertEquals( $dates['year'], 2016 );
-		$this->assertEquals( $dates['day_end'], 30 );
-		$this->assertEquals( $dates['m_end'], 9 );
-		$this->assertEquals( $dates['year_end'], 2016 );
+		$this->assertEquals( $dates['m_start'], date( 'n' ) );
+		$this->assertEquals( $dates['year'], date( 'Y' ) );
+		$this->assertEquals( $dates['day_end'], cal_days_in_month( CAL_GREGORIAN, $dates['m_start'], $dates['year'] ) );
+		$this->assertEquals( $dates['m_end'], date( 'n' ) );
+		$this->assertEquals( $dates['year_end'], date( 'Y' ) );
 
 	}
 
@@ -66,21 +52,16 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_get_report_dates_correct_this_month_at_the_beginning_of_the_month_utc() {
 
-		global $AFFWP_TEST_TIME, $AFFWP_TEST_UTC_OFFSET;
-
-		$AFFWP_TEST_TIME       = strtotime( '2016/09/01 00:00 UTC' );
-		$AFFWP_TEST_UTC_OFFSET = 0;
-
 		date_default_timezone_set( 'UTC' );
 
 		$dates = affwp_get_report_dates();
 
 		$this->assertEquals( $dates['day'], 1 );
-		$this->assertEquals( $dates['m_start'], 9 );
-		$this->assertEquals( $dates['year'], 2016 );
-		$this->assertEquals( $dates['day_end'], 30 );
-		$this->assertEquals( $dates['m_end'], 9 );
-		$this->assertEquals( $dates['year_end'], 2016 );
+		$this->assertEquals( $dates['m_start'], date( 'n' ) );
+		$this->assertEquals( $dates['year'], date( 'Y' ) );
+		$this->assertEquals( $dates['day_end'], cal_days_in_month( CAL_GREGORIAN, $dates['m_start'], $dates['year'] ) );
+		$this->assertEquals( $dates['m_end'], date( 'n' ) );
+		$this->assertEquals( $dates['year_end'], date( 'Y' ) );
 
 	}
 
@@ -89,35 +70,27 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_get_report_dates_correct_this_month_at_the_beginning_of_the_month_pdt() {
 
-		global $AFFWP_TEST_TIME, $AFFWP_TEST_UTC_OFFSET;
-
-		$AFFWP_TEST_TIME       = strtotime( '2016/09/01 00:00 UTC' );
-		$AFFWP_TEST_UTC_OFFSET = - 7;
-
 		date_default_timezone_set( 'America/Los_Angeles' );
 
 		$dates = affwp_get_report_dates();
 
 		$this->assertEquals( $dates['day'], 1 );
-		$this->assertEquals( $dates['m_start'], 8 );
-		$this->assertEquals( $dates['year'], 2016 );
-		$this->assertEquals( $dates['day_end'], 31 );
-		$this->assertEquals( $dates['m_end'], 8 );
-		$this->assertEquals( $dates['year_end'], 2016 );
+		$this->assertEquals( $dates['m_start'], date( 'n' ) );
+		$this->assertEquals( $dates['year'], date( 'Y' ) );
+		$this->assertEquals( $dates['day_end'], cal_days_in_month( CAL_GREGORIAN, $dates['m_start'], $dates['year'] ) );
+		$this->assertEquals( $dates['m_end'], date( 'n' ) );
+		$this->assertEquals( $dates['year_end'], date( 'Y' ) );
 
 	}
 
 	/**
 	 * @covers affwp_get_report_dates()
 	 */
-	public function test_get_report_dates_correct_this_moment() {
-
-		unset( $GLOBALS['AFFWP_TEST_TIME'] );
-		unset( $GLOBALS['AFFWP_TEST_UTC_OFFSET'] );
+	public function test_get_report_dates_correct_this_moment_utc() {
 
 		date_default_timezone_set( 'UTC' );
 
-		$current_time = current_time( 'timestamp', 1 );
+		$current_time = current_time( 'timestamp' );
 		$dates = affwp_get_report_dates();
 
 		$this->assertEquals( $dates['day'], 1 );

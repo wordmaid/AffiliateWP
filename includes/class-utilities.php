6ff3f07688ac25a97373f024a5b73c7af1ac21d1
Drew Jaynes <drew@affiliatewp.com>
2016-12-13 15:49:09 -0700

    Instantiate the utilities class in the plugin bootstrap.

---
+++ includes/class-utilities.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Utilities class for AffiliateWP.
+ *
+ * @since 2.0
+ */
+class Affiliate_WP_Utilities {
+
+	/**
+	 * Batch processor class instance variable.
+	 *
+	 * @access public
+	 * @since  2.0
+	 * @var    \AffWP\Util\Batch_Processor
+	 */
+	public $batch;
+
+	/**
+	 * Instantiates the utilities class.
+	 *
+	 * @access public
+	 * @since  2.0
+	 */
+	public function __construct() {
+		$this->includes();
+		$this->setup_objects();
+	}
+
+	/**
+	 * Includes necessary utility files.
+	 *
+	 * @access public
+	 * @since  2.0
+	 */
+	public function includes() {
+		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-batch-processor.php';
+	}
+
+	/**
+	 * Sets up utility objects.
+	 *
+	 * @access public
+	 * @since  2.0
+	 */
+	public function setup_objects() {
+		$this->batch = new AffWP\Util\Batch_Processor;
+	}
+}

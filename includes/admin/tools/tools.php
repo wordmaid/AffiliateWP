da10007bcc89424935565e4f188f8ae5e5d180ba
Drew Jaynes <DrewAPicture@users.noreply.github.com>
2016-12-27 09:32:37 -0700

    issue/1830 – Implement new primitive and meta caps (#1859) Fixes #1830
    * Check manage_payouts in the core payouts REST endpoints.
    
    * Only add and render the API Keys tab in the Tools screen if the current user can manage REST consumers.
    
    * Specifically tie importing settings to the manage_affiliate_options cap vs manage_options.
    
    * Only add the System Info tab in the Tools screen if the current user has the manage_affiliate_options cap.
    
    * Check the manage_payouts primitive cap when registering the Payouts submenu item.
    
    * Make a completely unrelated spacing change to class-menu.php because readability is useful.

--- includes/admin/tools/tools.php
+++ includes/admin/tools/tools.php
@@ -70,13 +70,20 @@ function affwp_get_tools_tabs() {
 
 	$tabs                  = array();
 	$tabs['export_import'] = __( 'Export / Import', 'affiliate-wp' );
-	$tabs['api_keys']      = __( 'API Keys', 'affiliate-wp' );
+
+	if ( current_user_can( 'manage_consumers' ) ) {
+		$tabs['api_keys'] = __( 'API Keys', 'affiliate-wp' );
+	}
+
 	$tabs['recount']       = __( 'Recount Stats', 'affiliate-wp' );
 	$tabs['migration']     = __( 'Migration Assistant', 'affiliate-wp' );
-	$tabs['system_info']   = __( 'System Info', 'affiliate-wp' );
+
+	if ( current_user_can( 'manage_affiliate_options' ) ) {
+		$tabs['system_info'] = __( 'System Info', 'affiliate-wp' );
+	}
 
 	if( affiliate_wp()->settings->get( 'debug_mode', false ) ) {
-		$tabs['debug']     = __( 'Debug Assistant', 'affiliate-wp' );
+		$tabs['debug'] = __( 'Debug Assistant', 'affiliate-wp' );
 	}
 
 	/**
@@ -473,6 +480,10 @@ add_action( 'admin_init', 'affwp_clear_debug_log' );
  * @since 1.9
  */
 function affwp_rest_api_keys_tab() {
+	if ( ! current_user_can( 'manage_consumers' ) ) {
+		return;
+	}
+
 	$keys_table = new \AffWP\REST\Admin\Consumers_Table;
 	$keys_table->prepare_items();
 

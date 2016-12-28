358cbb9f8de951ecb18f5d8f35aa34ccc66dd788
Andrew Munro <andrew@sumobi.com>
2016-12-28 18:31:53 +1300

    Add barebones class

---
+++ includes/integrations/class-caldera-forms.php
@@ -0,0 +1,18 @@
+<?php
+
+class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
+
+	/**
+	 * Get things started
+	 *
+	 * @access  public
+	 * @since   2.0
+	*/
+	public function init() {
+
+		$this->context = 'caldera-forms';
+
+	}
+
+}
+new Affiliate_WP_Caldera_Forms;

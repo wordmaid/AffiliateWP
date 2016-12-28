965358c514fa95a2862a13c3ef21f722b8143679
Andrew Munro <andrew@sumobi.com>
2016-12-28 18:36:08 +1300

    Add add_pending_referral() method

--- includes/integrations/class-caldera-forms.php
+++ includes/integrations/class-caldera-forms.php
@@ -12,6 +12,18 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 
 		$this->context = 'caldera-forms';
 
+		add_action( 'caldera_forms_submit_complete', array( $this, 'add_pending_referral' ), 10, 3 );
+
+	}
+
+	/**
+	 * Records a pending referral
+	 *
+	 * @access  public
+	 * @since   2.0
+	*/
+	public function add_pending_referral( $form, $referrer, $process_id ) {
+
 	}
 
 }

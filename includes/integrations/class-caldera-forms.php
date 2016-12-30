deba383d4c419ed519faa617219381b74e72ed53
Andrew Munro <andrew@sumobi.com>
2016-12-30 20:18:05 +1300

    Add processor to handle total

--- includes/integrations/class-caldera-forms.php
+++ includes/integrations/class-caldera-forms.php
@@ -12,11 +12,101 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 
 		$this->context = 'caldera-forms';
 
-		add_action( 'caldera_forms_entry_saved', array( $this, 'add_pending_referral' ), 10, 3 );
+		add_action( 'caldera_forms_entry_saved', array( $this, 'entry_saved' ), 10, 3 );
 		add_action( 'caldera_forms_general_settings_panel', array( $this, 'add_settings' ) );
+
+		add_action( 'caldera_forms_pre_load_processors', array( $this, 'pre_load_processors' ), 10, 6 );
+		add_action( 'caldera_forms_includes_complete', array( $this, 'load_processor' ), 10, 6 );
 
 		add_action( 'cf_stripe_post_successful_charge', array( $this, 'complete_payment_stripe' ), 10, 4 );
 		add_action( 'cf_braintree_success', array( $this, 'complete_payment_braintree' ), 10, 6 );
+
+	}
+
+	/**
+	 * Load the processor
+	 *
+	 * @access  public
+	 * @since   2.0
+	 */
+	public function load_processor() {
+
+		if ( class_exists( 'Caldera_Forms_Processor_Processor' ) ) {
+			require_once ( AFFILIATEWP_PLUGIN_DIR . 'includes/integrations/extras/class-caldera-forms-processor.php' );
+		}
+
+	}
+
+	/**
+	 * Register processor when in Caldera Forms admin, or when rendering form.
+	 *
+	 * @access public
+	 * @since  2.0
+	 */
+	public function pre_load_processors() {
+
+	    $config = array(
+	        'name'        => 'AffiliateWP',
+	        'author'      => 'AffiliateWP',
+	        'description' => 'Create a referral in AffiliateWP',
+	        'author_url'  => 'https://affiliatewp.com',
+			'icon'        => AFFILIATEWP_PLUGIN_URL . 'assets/images/logo-affwp.svg',
+	        'template'    => AFFILIATEWP_PLUGIN_DIR . 'includes/integrations/extras/caldera-forms-config.php'
+	    );
+
+	    new AffiliateWP_Caldera_Forms_Processor( $config, $this->fields(), 'affwp' );
+
+	}
+
+	/**
+	 * Fields shown for the affwp processor
+	 *
+	 * @access public
+	 * @since  2.0
+	 */
+	public function fields() {
+
+		return array(
+			array(
+				'id'       => 'total',
+				'label'    => __( 'Total', 'affiliate-wp' ),
+				'type'     => 'text',
+				'required' => true,
+				'magic'    => true,
+			)
+		);
+
+	}
+
+	/**
+	 * Records a 0.00 referral when the entry is saved
+	 *
+	 * @access  public
+	 * @since   2.0
+	*/
+	public function entry_saved( $entry_id, $new_entry, $form ) {
+
+		/**
+		 * Forms without the affwp processor will generate a $0.00 referral
+		 * Forms with the affwp processor are handled via the AffiliateWP_Caldera_Forms_Processor class and will not go any further than this
+		 */
+		if ( $form['processors'] ) {
+			foreach ( $form['processors'] as $processor ) {
+				if ( $processor['type'] === 'affwp' ) {
+					// return if form has affwp processor
+					return false;
+				}
+			}
+		}
+
+		$args = array(
+			'entry_id'               => $entry_id,
+			'referral_total'         => 0.00,
+			'mark_referral_complete' => true
+		);
+
+		// Add pending referral
+		$this->add_pending_referral( $args, $form );
 
 	}
 
@@ -25,10 +115,11 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 	 *
 	 * @access  public
 	 * @since   2.0
-	*/
-	public function add_pending_referral( $entry_id, $new_entry, $form ) {
+	 */
+	public function add_pending_referral( $args = array(), $form ) {
 
 		$affiliate_id = $this->affiliate_id;
+		$entry_id     = $args['entry_id'];
 
 		// Return if the customer was not referred or the affiliate ID is empty
 		if ( ! $this->was_referred() && empty( $affiliate_id ) ) {
@@ -53,18 +144,17 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 			return false;
 		}
 
-		// get referral total
-		$total          = $this->get_field_value( 'calculation', $form );
-		$referral_total = $this->calculate_referral_amount( $total, $entry_id );
+		// Referral total
+		$referral_total = $args['referral_total'];
 
-		// use form title as description
+		// Use form title as description
 		$description = $form['name'];
 
-		// insert a pending referral
+		// Insert a pending referral
 		$referral_id = $this->insert_pending_referral( $referral_total, $entry_id, $description );
 
-		// Mark referral complete (unpaid) if no total
-		if ( empty( $referral_total ) ) {
+		// Mark referral complete (set to "unpaid" status)
+		if ( isset( $args['mark_referral_complete'] ) ) {
 			$this->mark_referral_complete( $entry_id );
 		}
 
@@ -74,7 +164,7 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 	 * Mark referral as "unpaid" when the payment is successful in Stripe
 	 *
 	 * @access public
-	 * @since 2.0
+	 * @since  2.0
 	 */
 	public function complete_payment_stripe( $return_charge, $transdata, $config, $form ) {
 
@@ -88,7 +178,7 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 	 * Mark referral as "unpaid" when the payment is successful in Braintree
 	 *
 	 * @access public
-	 * @since 2.0
+	 * @since  2.0
 	 */
 	public function complete_payment_braintree( $result, $order_id, $transaction, $config, $form, $proccesid ) {
 
@@ -109,9 +199,10 @@ class Affiliate_WP_Caldera_Forms extends Affiliate_WP_Base {
 	}
 
 	/**
-	 * Get calculation field total
+	 * Get a field's value
 	 *
-	 * @since 2.0
+	 * @access public
+	 * @since  2.0
 	 */
 	public function get_field_value( $type = '', $form ) {
 
---
+++ includes/integrations/extras/class-caldera-forms-processor.php
@@ -0,0 +1,50 @@
+<?php
+
+class AffiliateWP_Caldera_Forms_Processor extends Caldera_Forms_Processor_Processor {
+
+	/**
+	 * Pre Processor
+	 *
+	 * @access public
+	 * @since 2.0
+	 */
+    public function pre_processor( array $config, array $form, $process_id ) {
+        // This method must exist to avoid an error
+        // Using it would be bad as payments will not have been attempted yet.
+    }
+
+	/**
+	 * Processor
+	 * Payments will have been processed by now, if they generated an error, this method will not run.
+	 *
+	 * @access public
+	 * @since 2.0
+	 */
+    public function processor( array $config, array $form, $process_id ) {
+
+		$integration  = new Affiliate_WP_Caldera_Forms;
+
+		// Get entry ID
+		$submission_data = Caldera_Forms::get_submission_data( $form );
+		$entry_id        = $submission_data['_entry_id'];
+
+		// Get values of all settings for this processor
+        $this->set_data_object_initial( $config, $form );
+
+        // Get total conversion value set in processor whether it is hardcoded or field value
+        $total = $this->data_object->get_value( 'total' );
+
+		// Get referral total
+		$referral_total = $integration->calculate_referral_amount( $total, $entry_id );
+
+		$args = array(
+			'entry_id'               => $entry_id,
+			'referral_total'         => $referral_total,
+			'mark_referral_complete' => true
+		);
+
+		$integration->add_pending_referral( $args, $form );
+
+    }
+
+}

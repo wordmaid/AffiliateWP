6cbada3fb8ed4712c0d3574117b03c87961aa0f6
Andrew Munro <andrew@sumobi.com>
2017-01-01 00:17:50 +1300

    Add barebones give class

---
+++ includes/integrations/class-give.php
@@ -0,0 +1,20 @@
+<?php
+
+class Affiliate_WP_Give extends Affiliate_WP_Base {
+
+	/**
+	 * Get things started
+	 *
+	 * @access  public
+	 * @since   1.0
+	*/
+	public function init() {
+
+		$this->context = 'give';
+
+
+	}
+
+
+}
+new Affiliate_WP_Give;

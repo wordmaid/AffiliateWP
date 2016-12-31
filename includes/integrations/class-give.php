7dcbf9d6cf94c1c1b4d25b6c791449a817886543
Andrew Munro <andrew@sumobi.com>
2017-01-01 00:56:07 +1300

    Add add_pending_referral() method

--- includes/integrations/class-give.php
+++ includes/integrations/class-give.php
@@ -6,15 +6,25 @@ class Affiliate_WP_Give extends Affiliate_WP_Base {
 	 * Get things started
 	 *
 	 * @access  public
-	 * @since   1.0
+	 * @since   2.0
 	*/
 	public function init() {
 
 		$this->context = 'give';
 
+		add_action( 'give_insert_payment', array( $this, 'add_pending_referral' ), 99999, 2 );
 
 	}
 
+	/**
+	 * Records a pending referral when a pending payment is created
+	 *
+	 * @access  public
+	 * @since   2.0
+	*/
+	public function add_pending_referral( $payment_id = 0, $payment_data = array() ) {
+
+	}
 
 }
 new Affiliate_WP_Give;

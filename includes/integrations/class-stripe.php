0df60612d38e29512cb697f8d2245a955381d7fe
Pippin Williamson <pippin@pippinsplugins.com>
2017-01-05 10:09:54 -0600

    Rework Stripe integration to use new simpay_charge_created action. #1500

--- includes/integrations/class-stripe.php
+++ includes/integrations/class-stripe.php
@@ -12,8 +12,7 @@ class Affiliate_WP_Stripe extends Affiliate_WP_Base {
 
 		$this->context = 'stripe';
 
-		add_filter( 'sc_meta_values', array( $this, 'maybe_insert_referral' ) );
-		add_filter( 'sc_redirect_args', array( $this, 'mark_referral_complete' ), 10, 2 );
+		add_action( 'simpay_charge_created', array( $this, 'insert_referral' ), 10, 2 );
 
 		add_filter( 'affwp_referral_reference_column', array( $this, 'reference_link' ), 10, 2 );
 
@@ -26,19 +25,17 @@ class Affiliate_WP_Stripe extends Affiliate_WP_Base {
 	 * @access  public
 	 * @since   2.0
 	*/
-	public function maybe_insert_referral( $meta ) {
+	public function insert_referral( $charge ) {
 
 		if( $this->was_referred() ) {
 
-			$token       = sanitize_text_field( $_POST['stripeToken'] );
-			$amount      = round( sanitize_text_field( $_POST['sc-amount'] ) / 100, 2 );
-			$email       = sanitize_text_field( $_POST['stripeEmail'] );
-			$description = sanitize_text_field( $_POST['sc-description'] );
+			if( $this->is_zero_decimal( $charge->currency ) ) {
+				$amount = $charge->amount;
+			} else {
+				$amount = round( $charge->amount / 100, 2 );
+			}
 
-			// This is used to look up the referral after completion
-			$_POST['affwpStripeToken'] = $token;
-
-			if( $this->is_affiliate_email( $email, $this->affiliate_id ) ) {
+			if( $this->is_affiliate_email( $charge->customer->email, $this->affiliate_id ) ) {
 
 				if( $this->debug ) {
 					$this->log( 'Referral not created because affiliate\'s own account was used.' );
@@ -48,56 +45,58 @@ class Affiliate_WP_Stripe extends Affiliate_WP_Base {
 
 			}
 
-			$referral_total = $this->calculate_referral_amount( $amount, $token );
-
-			$referral_id = $this->insert_pending_referral( $referral_total, $token, $description );
+			$referral_total = $this->calculate_referral_amount( $amount, $charge->id );
+			$referral_id    = $this->insert_pending_referral( $referral_total, $charge->id, $charge->description, array(), array( 'livemode' => $charge->livemode ) );
 
 			if( $referral_id && $this->debug ) {
 
-				$this->log( 'Pending referral created successfully during maybe_insert_referral()' );
+				$this->log( 'Pending referral created successfully during insert_referral()' );
 
 			} elseif ( $this->debug ) {
 
-				$this->log( 'Pending referral failed to be created during maybe_insert_referral()' );
+				$this->log( 'Pending referral failed to be created during insert_referral()' );
+
+			}
+
+			if( $this->complete_referral( $charge->id ) && $this->debug ) {
+
+				$this->log( 'Referral completed successfully during insert_referral()' );
 
 			}
 
 		}
-
-		return $meta;
 
 	}
 
 	/**
-	 * Mark referral complete
+	 * Determine if this is a zero decimal currency
 	 *
-	 * @function mark_referral_complete()
 	 * @access public
-	 * @return array
+	 * @since  2.0
+	 * @param  $currency String The currency code
+	 * @return bool
 	 */
-	public function mark_referral_complete( $query_args, $charge ) {
+	public function is_zero_decimal( $currency ) {
 
-		if( $this->was_referred() && empty( $query_args['charge_failed'] ) && ! empty( $_POST['affwpStripeToken'] ) ) {
+		$is_zero = array(
+			'BIF',
+			'CLP',
+			'DJF',
+			'GNF',
+			'JPY',
+			'KMF',
+			'KRW',
+			'MGA',
+			'PYG',
+			'RWF',
+			'VND',
+			'VUV',
+			'XAF',
+			'XOF',
+			'XPF',
+		);
 
-			$token = sanitize_text_field( $_POST['affwpStripeToken'] );
-
-			$referral = affiliate_wp()->referrals->get_by( 'reference', $token, $this->context );
-			$referral = affwp_get_referral( $referral );
-
-			if( $referral ) {
-
-				$referral->reference = $charge->id;
-				$referral->custom    = $token;
-				$referral->save();
-
-			}
-
-			$this->complete_referral( $charge->id );
-
-		}
-
-		return $query_args;
-
+		return in_array( strtoupper( $currency ), $iz_zero );
 	}
 
 	/**
@@ -114,7 +113,14 @@ class Affiliate_WP_Stripe extends Affiliate_WP_Base {
 
 		}
 
-		$url = 'https://dashboard.stripe.com/payments/' . $reference ;
+		$test = '';
+
+		if( ! empty( $referral->custom ) ) {
+			$custom = maybe_unserialize( $referral->custom );
+			$test   = empty( $custom['livemode'] ) ? 'test/' : '';
+		}
+
+		$url = 'https://dashboard.stripe.com/' . $test . 'payments/' . $reference ;
 
 		return '<a href="' . esc_url( $url ) . '">' . $reference . '</a>';
 	}

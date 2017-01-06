d387e5a750016bc3e88689dc503745f135c9da88
Drew Jaynes <drew@affiliatewp.com>
2017-01-06 13:44:48 -0700

    Add tests for ::global_license_set() and ::get_license_key().

--- tests/admin/test-settings.php
+++ tests/admin/test-settings.php
@@ -13,20 +13,33 @@ class Tests extends UnitTestCase {
 	/**
 	 * Settings instance.
 	 *
-	 * @access public
-	 * @var Affiliate_WP_Settings
+	 * @access protected
+	 * @var    Affiliate_WP_Settings
+	 * @static
 	 */
-	public $settings;
+	protected static $settings;
+
+	/**
+	 * Set up fixtures once.
+	 */
+	public static function wpSetUpBeforeClass() {
+		self::$settings = new \Affiliate_WP_Settings;
+	}
 
 	/**
 	 * Tests set up.
 	 */
 	public function setUp() {
-		parent::setUp();
-
-		$this->settings = new \Affiliate_WP_Settings();
-
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/settings/display-settings.php';
+	}
+
+	/**
+	 * Tests tear down.
+	 */
+	public function tearDown() {
+		affiliate_wp()->settings->set( array( 'license_key' => '' ), true );
+
+		parent::tearDown();
 	}
 
 	/**
@@ -34,11 +47,11 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_set_method_should_update_options_property_in_memory_only() {
 		$affiliates_area = 100000;
-		$this->settings->set( array(
+		self::$settings->set( array(
 			'affiliates_page' => $affiliates_area
 		) );
 
-		$options = $this->settings->get_all();
+		$options = self::$settings->get_all();
 		$actual  = get_option( 'affwp_settings', array() );
 
 		$this->assertSame( $options['affiliates_page'], $affiliates_area );
@@ -50,11 +63,11 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_set_method_with_save_trigger_should_update_settings() {
 		$affiliates_area = rand( 1, 200 );
-		$this->settings->set( array(
+		self::$settings->set( array(
 			'affiliates_page' => $affiliates_area
 		), $save = true );
 
-		$options = $this->settings->get_all();
+		$options = self::$settings->get_all();
 		$actual  = get_option( 'affwp_settings', array() );
 
 		$this->assertSame( $options['affiliates_page'], $affiliates_area );
@@ -66,18 +79,85 @@ class Tests extends UnitTestCase {
 	 */
 	public function test_is_setting_disabled() {
 		// Default should be false.
-		$this->assertFalse( $this->settings->get( 'debug_mode' ) );
+		$this->assertFalse( self::$settings->get( 'debug_mode' ) );
 
 		// Define and reset $settings.
 		define( 'AFFILIATE_WP_DEBUG', true );
-		$this->settings = new \Affiliate_WP_Settings();
+		self::$settings = new \Affiliate_WP_Settings();
 
 		// Constant should override the value.
-		$this->assertTrue( $this->settings->get( 'debug_mode' ) );
+		$this->assertTrue( self::$settings->get( 'debug_mode' ) );
 
-		$registered_settings = $this->settings->get_registered_settings();
+		$registered_settings = self::$settings->get_registered_settings();
 		$args = array_merge( $registered_settings['misc']['debug_mode'], array( 'id' => 'debug_mode' ) );
 
-		$this->assertTrue( $this->settings->is_setting_disabled( $args ) );
+		$this->assertTrue( self::$settings->is_setting_disabled( $args ) );
 	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::global_license_set()
+	 *
+	 * Note: This test needs to happen before the global license constant is defined
+	 * within the scope of the test suite.
+	 */
+	public function test_global_license_set_where_constant_is_not_defined_should_return_false() {
+		$this->assertFalse( \Affiliate_WP_Settings::global_license_set() );
+	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::get_license_key()
+	 */
+	public function test_get_license_key_with_empty_key_and_saving_false_should_return_the_saved_key() {
+		affiliate_wp()->settings->set( array( 'license_key' => 'baz' ) );
+
+		$this->assertSame( 'baz', \Affiliate_WP_Settings::get_license_key() );
+	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::get_license_key()
+	 */
+	public function test_get_license_key_with_empty_key_and_saving_true_should_return_empty() {
+		affiliate_wp()->settings->set( array( 'license_key' => 'foo' ) );
+
+		$this->assertSame( '', \Affiliate_WP_Settings::get_license_key( '', true ) );
+	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::get_license_key()
+	 */
+	public function test_get_license_key_with_not_empty_key_and_saving_true_should_return_key() {
+		$this->assertSame( 'bar', \Affiliate_WP_Settings::get_license_key( 'bar', true ) );
+	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::get_license_key()
+	 */
+	public function test_get_license_key_with_not_empty_key_and_no_constant_and_savind_false_should_return_that_key() {
+		$this->assertSame( 'foobar', \Affiliate_WP_Settings::get_license_key( 'foobar' ) );
+	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::get_license_key()
+	 *
+	 * Note: This test needs to occur toward the end of the class so as not
+	 * to unexpectedly pollute other tests.
+	 */
+	public function test_get_license_key_with_global_key_defined_should_return_that_key() {
+		define( 'AFFILIATEWP_LICENSE_KEY', 'foobar' );
+
+		static $settings;
+		$settings = new \Affiliate_WP_Settings;
+
+		$this->assertSame( 'foobar', $settings::get_license_key() );
+	}
+
+	/**
+	 * @covers \Affiliate_WP_Settings::global_license_set()
+	 *
+	 * Note: This test must fall after the constant in the previous test has been set.
+	 */
+	public function test_global_license_set_where_constant_is_defined_and_not_empty_should_return_true() {
+		$this->assertTrue( \Affiliate_WP_Settings::global_license_set() );
+	}
+
 }

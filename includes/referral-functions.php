3bd009249080df6f88eb396acd9997931790c4d1
Drew Jaynes <drew@affiliatewp.com>
2017-01-09 21:56:23 -0700

    Ensure decreasing unpaid earnings is also handled in affwp_delete_referral().

--- includes/referral-functions.php
+++ includes/referral-functions.php
@@ -251,14 +251,20 @@ function affwp_delete_referral( $referral ) {
 		return false;
 	}
 
-	if( $referral && 'paid' == $referral->status ) {
+	if ( $referral ) {
+		if ( 'paid' === $referral->status ) {
+			// This referral has already been paid, so decrease the affiliate's earnings
+			affwp_decrease_affiliate_earnings( $referral->affiliate_id, $referral->amount );
 
-		// This referral has already been paid, so decrease the affiliate's earnings
-		affwp_decrease_affiliate_earnings( $referral->affiliate_id, $referral->amount );
+			// Decrease the referral count
+			affwp_decrease_affiliate_referral_count( $referral->affiliate_id );
 
-		// Decrease the referral count
-		affwp_decrease_affiliate_referral_count( $referral->affiliate_id );
+		} elseif ( 'unpaid' === $referral->status ) {
 
+			// Decrease the unpaid earnings.
+			affwp_decrease_affiliate_unpaid_earnings( $referral->affiliate_id, $referral->amount );
+
+		}
 	}
 
 	if( affiliate_wp()->referrals->delete( $referral->ID, 'referral' ) ) {
